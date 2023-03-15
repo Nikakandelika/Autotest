fe1f61d98244d696c40da7c02502cca785618086
Nika <vdubrova442@gmail.com>
2023-03-15 15:24:45 +0300

    add readme

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,14 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+def page(a, b, c):
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-username = driver.find_element(By.CSS_SELECTOR, "#user-name")
-password = driver.find_element(By.CSS_SELECTOR, "#password")
-submit = driver.find_element(By.CSS_SELECTOR, "#login-button")
-list = [username, password, submit]
-for i in range(len(list)):
-    if list[i] is None:
-        print("Элемент не найден")
-    else:
-        print("Элемент найден")
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    username = driver.find_elements(By.CSS_SELECTOR, a)
+    password = driver.find_elements(By.CSS_SELECTOR, b)
+    submit = driver.find_elements(By.CSS_SELECTOR, c)
+    list = [a, b, c]
+    for i in range(len(list)):
+        if list[i] is None:
+            return "Элемент не найден"
+        else:
+            return "Элемент найден"
+
+print(page("user-name", "password", "login-button"))
